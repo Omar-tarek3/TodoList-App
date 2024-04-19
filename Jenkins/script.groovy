da99f95addfa7be418327fb7f40deb27daa77f6d
Omar-tarek3 <omartarekabdelall@gmail.com>
2024-04-19 23:34:47 +0200

    fix

--- Jenkins/script.groovy
+++ Jenkins/script.groovy
@@ -5,10 +5,10 @@ def buildImage() {
     withCredentials([usernamePassword(
         credentialsId:'Dockerhub', passwordVariable:'dockerhubPass', usernameVariable:'dockerhubUser')]){
 
-        sh 'docker build -t omartarekabdelall/todo-list-app:frontend-jenkins ./Frontend'
-        sh "echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin"         
-        sh "docker push omartarekabdelall/todo-list-app:frontend-jenkins"
-        //sh "docker pull omartarekabdelall/todo-list-app:frontend"
+            sh 'docker build -t omartarekabdelall/todo-list-app:frontend-jenkins ./Frontend'
+            sh "echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin"         
+            sh "docker push omartarekabdelall/todo-list-app:frontend-jenkins"
+            //sh "docker pull omartarekabdelall/todo-list-app:frontend"
         }                       
 }
 
