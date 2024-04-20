5518dc056686cbcdb1ff217c6c1dc0ebfeb7b159
Omar-tarek3 <omartarekabdelall@gmail.com>
2024-04-20 14:55:31 +0200

    adding backend and database images

--- Jenkins/script.groovy
+++ Jenkins/script.groovy
@@ -1,19 +1,37 @@
+
 def buildImage() {
-    
-    echo 'Building Frontend image'
-                    
+                  
     withCredentials([usernamePassword(
         credentialsId:'Dockerhub', passwordVariable:'dockerhubPass', usernameVariable:'dockerhubUser')]){
-
+            
+            echo 'Building Frontend image'
             sh 'docker build -t omartarekabdelall/todo-list-app:frontend-jenkins ./Frontend'
+
+            echo 'Building Backend image'
+            sh 'docker build -t omartarekabdelall/todo-list-app:backend-jenkins ./Backend'
+
+            echo 'Building Database image'
+            sh 'docker build -t omartarekabdelall/todo-list-app:database-jenkins ./Database'
+
             sh "echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin"         
             sh "docker push omartarekabdelall/todo-list-app:frontend-jenkins"
             //sh "docker pull omartarekabdelall/todo-list-app:frontend"
         }                       
 }
 
-def deployApp(){
-    echo "Building Frontend Image"
+def pushImage(){
+
+    echo "Pushing Images to Dockerhub Repo"
+
+     withCredentials([usernamePassword(
+        credentialsId:'Dockerhub', passwordVariable:'dockerhubPass', usernameVariable:'dockerhubUser')]){
+
+            sh "echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin"         
+            sh "docker push omartarekabdelall/todo-list-app:frontend-jenkins"
+            sh "docker push omartarekabdelall/todo-list-app:backend-jenkins"
+            sh "docker push omartarekabdelall/todo-list-app:database-jenkins"
+        } 
+    
 
 }
 
